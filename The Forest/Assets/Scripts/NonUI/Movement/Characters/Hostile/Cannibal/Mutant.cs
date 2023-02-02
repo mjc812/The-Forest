103d6b5a92409d3ec6889b675f42e62e11a2fd08
mjc812 <68936604+mjc812@users.noreply.github.com>
2023-02-01 22:06:13 -0800

    implement creature 4 into scene with preliminary implementation of animation

---
+++ The Forest/Assets/Scripts/NonUI/Movement/Characters/Hostile/Cannibal/Mutant.cs
@@ -0,0 +1,124 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mutant : MonoBehaviour
+{
+    private enum State
+    {
+        IDLE,
+        WALK,
+        CHASE,
+        ATTACK
+    }
+
+    private Animator animator;
+    private NavMeshAgent navMeshAgent;
+    private Transform player;
+
+    private State movingState;
+
+    private float chaseSpeed = 2.0f;
+    private float attackDistance = 4f;
+    private float stopNavMeshAgentDistance = 1.8f;
+    public float rotationSpeed = 10f;
+   
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player").transform;
+
+        movingState = State.CHASE;
+        navMeshAgent.updateRotation = false;
+    }
+
+    void Start() {
+        //animator.SetBool("Walk", true);
+    }
+
+    void Update()
+    {
+        switch (movingState)
+        {
+            case State.CHASE:
+                {
+                    Chase();
+                    break;
+                }
+            case State.ATTACK:
+                {
+                    Attack();
+                    break;
+                }
+        }
+    }
+
+    private void Chase()
+    {
+        navMeshAgent.isStopped = false;
+        navMeshAgent.SetDestination(player.position);
+        navMeshAgent.speed = chaseSpeed;
+        rotateTowardsDirection();
+        animator.SetBool("Run", true);
+
+        if (CheckAttackDistance())
+        {
+            animator.SetBool("Run", false);
+            movingState = State.ATTACK;
+        }
+    }
+
+    private float attackTime = 1.5f;
+    private float attackTimeTotal = 0f;
+
+    private void Attack()
+    {
+//        if (CheckNavMeshAgentDistance()) {
+            navMeshAgent.isStopped = true;
+            navMeshAgent.velocity = Vector3.zero;
+//        }
+
+        RotateTowardsPlayer();
+
+        attackTimeTotal += Time.deltaTime;
+        if (attackTimeTotal >= attackTime) {
+            attackTimeTotal = 0f;
+            Debug.Log("attacking");
+            animator.SetTrigger("Attack");
+        }
+
+        if (!CheckAttackDistance())
+        {
+            attackTimeTotal = 0f;
+            movingState = State.CHASE;
+        }
+    }
+
+    private bool CheckAttackDistance()
+    {
+        float dist = Vector3.Distance(player.position, transform.position);
+        return dist <= attackDistance;
+    }
+
+    private bool CheckNavMeshAgentDistance()
+    {
+        float dist = Vector3.Distance(player.position, transform.position);
+        return dist <= stopNavMeshAgentDistance;
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    private void rotateTowardsDirection()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        if (navMeshAgent.velocity.normalized != Vector3.zero) {
+            Quaternion lookRotation = Quaternion.LookRotation(navMeshAgent.velocity.normalized);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);   
+        }
+    }
+}
