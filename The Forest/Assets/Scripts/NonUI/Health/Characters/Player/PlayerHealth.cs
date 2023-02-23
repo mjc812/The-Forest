6689c15e22c566eee09b48fb8523da150ce893bb
mjc812 <68936604+mjc812@users.noreply.github.com>
2023-02-23 15:44:35 -0800

    add hit sound effects for player

--- The Forest/Assets/Scripts/NonUI/Health/Characters/Player/PlayerHealth.cs
+++ The Forest/Assets/Scripts/NonUI/Health/Characters/Player/PlayerHealth.cs
@@ -5,6 +5,9 @@ public class PlayerHealth : Health
 {
     private AudioSource audioSource; 
 
+    public float a = 0.7f;
+    public float b = 0.3f;
+
     public AudioClip[] audioClips;
     public KickShake.Params leftShakeParams;
     public KickShake.Params rightShakeParams;
@@ -25,6 +28,10 @@ public class PlayerHealth : Health
     }
     
     protected override void DamageEffects(float amount, bool isCentral, bool isLeft, bool isRight) {
+        audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+        audioSource.pitch = a;
+        audioSource.volume = b;
+        audioSource.Play();
         Vector3 sourcePosition = transform.position;
         if (isLeft) {
             CameraShaker.Shake(new KickShake(leftShakeParams, new CameraShake.Displacement(sourcePosition)));
